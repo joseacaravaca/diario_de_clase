b8ca4fec0fc8421db0d4110a8e94ddb5598aef6a
Jose Antonio Caravaca <joseacaravaca@gmail.com>
2024-12-17 21:07:24 +0100

    Cambios

--- copias.py
+++ copias.py
@@ -87,7 +87,7 @@ copies = int(copies) if copies.isdigit() else 40
 
 
 # Crear el PDF con portada, páginas numeradas y 
-# trasera, incluyendo una linea de pie cin frases del archivo frases csv
+# trasera, incluyendo una linea de pie con frases del archivo frases csv
 # en A5
 create_numbered_pdf(input_pdf_path, portada_path, trasera_path, output_pdf_path, copies, positions)
 print(f"PDF generado: {output_pdf_path}")
